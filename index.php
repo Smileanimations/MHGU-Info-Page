55b5f1a9e1fc025021cb019adf4c90bac1044d3c
Roald <nihilzero28@gmail.com>
2024-10-09 11:24:26 +0200

    changed filetype

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="container">
+    <video id="video" src="Images/UI/WebsiteTrailer.mp4" autoplay="" loop="" muted=""></video>
+    <div id="logo-section"> <img src="Images/UI/Logo-MHGU.webp" alt=""></div>
+</div>
+<div>
+    
+</div>
+<div id="buy-section">
+    <div>
+        <h1>Hunt Anywhere!</h1>
+        <h1>Hunt Anyway!</h1>
+        <h1>Hunt Ultimate!</h1>
+    </div>
+</div>
+</body>
+</html>
