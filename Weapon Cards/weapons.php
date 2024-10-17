042103c345c8249bbd0223315fb42b3e7b1a5437
Roald <nihilzero28@gmail.com>
2024-10-17 09:52:11 +0200

    added query properly and fixed wrongly copied code

--- Weapon Cards/weapons.php
+++ Weapon Cards/weapons.php
@@ -1,13 +1,9 @@
 <?php
-
 include_once("../connection.php");
 
 $icon;
-$query = $conn->query("SELECT * FROM `monsters`");
-$monsters = $query->fetchAll(PDO::FETCH_ASSOC);
-
-$red = "red";
-$green = "green"
+$query = $conn->query("SELECT * FROM `weapons`");
+$weapons = $query->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 <!DOCTYPE html>
@@ -15,10 +11,18 @@ $green = "green"
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Weapons</title>
+    <title>Weapon details</title>
 </head>
 <body>
-<a href="card.php">&crarr;Back</a>
-
+    <a href="card.php">&crarr;Back</a>
+    <ul class="card-list">
+        <?php foreach ($weapons as $index => $weapon) { ?>
+            <div class="card">
+                <div class="wrapper">
+                    <img src="../Images/Weapons/Weapon Renders/<?= $weapon['name']?>.webp" onerror="this.onerror=null; this.src='../Images/Monster Icons/Default_Icon.webp';" class="cover-image" alt="<?=$weapon['name']?>" />
+                </div>
+            </div>
+        <?php }?>
+    </ul>
 </body>
 </html>
