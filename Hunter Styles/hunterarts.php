62f9f6f5ce6664b5a4d8b1a35c4dbab97234c556
Roald <nihilzero28@gmail.com>
2024-10-17 13:56:01 +0200

    added navbar include

--- Hunter Styles/hunterarts.php
+++ Hunter Styles/hunterarts.php
@@ -1,7 +1,9 @@
 <?php
+include_once("../connection.php");
+include_once("../nav.html");
 
-
-
+$query = $conn->query("SELECT * FROM `hunter arts`");
+$weapons = $query->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 <!DOCTYPE html>
@@ -12,7 +14,6 @@
     <title>Hunter Arts</title>
 </head>
 <body>
-<a href="../index.html">&crarr;Back</a>
     <a>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod optio sed iste facilis libero, non odit necessitatibus? Neque repellendus esse expedita sapiente voluptates voluptate voluptas deserunt maiores nostrum perspiciatis? Sequi!</a>
 </body>
 </html>
