acec1d450e510085262a531b3439295525013022
Mark <iksnij12@gmail.com>
2024-10-17 10:36:22 +0200

    monster page start

--- Monsters/monsters.php
+++ Monsters/monsters.php
@@ -1,24 +1,10 @@
 <?php
 
-include_once("connection.php");
+include_once("../connection.php");
 
-$icon;
-$query = $conn->query("SELECT * FROM `monsters`");
+$query = $conn->query("SELECT * FROM `monsters` WHERE id=" . $_GET['id']);
 $monsters = $query->fetchAll(PDO::FETCH_ASSOC);
-
-$red = "red";
-$green = "green"
 ?>
-
-<style>
-img {
-    display: block;
-    max-width: 700px;
-    max-height:525px;
-    width: auto;
-    height: auto;
-}
-</style>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -26,26 +12,17 @@ img {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="monsters.css">
 </head>
 <body>
 <a href="index.html">&crarr;Back</a>
-    <?php foreach ($monsters as $index => $monster) {
-        ?>
-        <div style="border: 10px solid black; position: relative">
-            <img src="Images/Monster Icons/<?= str_replace(" ", "_", $monster['name'])?>_Icon.webp" onerror="this.onerror=null; this.src='../Images/Monster Icons/Default_Icon.webp';" alt=<?= $monster['name'];?>>
-            <h2 style="color:<?php if ($monster['deviant'] == 1){echo "red";} elseif ($monster['type'] == "Elder Dragon"){echo $green;}?>"><?=$monster['name'];?></h2>
-            <h3><?=$monster['type'];?></h3>
-            <p><?= $monster['description'];?></p>
-            <img src="images/Monster Renders/<?= str_replace(" ", "_", $monster['name'])?>_Render.webp" alt="">
-            <div>
-                <?php if (isset($monster['related_monsters'])) {
-                $related_monsters = explode(", ", $monster['related_monsters']);?>
-                <h4>Related Monsters</h4>
-                <?php foreach ($related_monsters as $key => $related_monster) {?>
-                    <img src="Images/Monster Icons/<?= str_replace(" ", "_", $related_monster)?>_Icon.webp" onerror="this.onerror=null; this.src='Images/Monster Icons/Default_Icon.webp';" alt="">
-                    <p><?= $related_monster?></p>
-                <?php }}?>
-            </div>
+    <?php foreach ($monsters as $index => $monster) { ?>
+        <div class="title">
+        <img src="../Images/Monster Icons/<?=str_replace(" ", "_", $monster['name'])?>_Icon.webp" onerror="this.onerror=null; this.src='../Images/Monster Icons/Default_Icon.webp';" class="cover-image" alt="<?=$monster['name']?>" />
+        <div class="ragingbrachy">
+            <h1><?=$monster['name']?></h1>
+            <h3><?=$monster['type']?></h3>
+        </div>
         </div>
     <?php }?>
 </body>
