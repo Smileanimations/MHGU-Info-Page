f6f953d272d8814e8ec5bdbf9860c0df1a46e007
Roald <nihilzero28@gmail.com>
2024-10-22 10:48:10 +0200

    added placeholder table

--- Hunter Styles/hunterarts.php
+++ Hunter Styles/hunterarts.php
@@ -12,12 +12,34 @@ $arts = $query->fetchAll(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hunter Arts</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <div>
+    <!-- <div>
         <?php foreach ($arts as $index => $art) { ?>
             <h1><?= $art["name"] ?></h1>
         <?php }?>
+    </div> -->
+    <h1 class="title" style="color: black;">Hunter Arts</h1>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Visual</th>
+            <th>Name</th>
+            <th>Description</th>
+            <!-- <th>Details</th> -->
+          </tr>
+        </thead>
+        <?php foreach ($arts as $index => $art) { ?>
+          <tr>
+            <td><img src="../Images/Weapons/Arts/<?php echo $art['name']?>.png" alt="" class="icons"></td>
+            <td><?= $art["name"] ?></td>
+            <td><?= $art["info"] ?></td>
+            <!-- <td><a href="details.php?id=<?= $art["id"] ?>">View details</a></td> -->
+          </tr>
+        <?php } ?>
+      </table>
     </div>
 </body>
 </html>
