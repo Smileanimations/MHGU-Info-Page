d3e44403c32ae76745c3eff3480f8466a01c0cd8
Mark <iksnij12@gmail.com>
2024-10-23 14:23:18 +0200

    Push these ba-

--- Monsters/monsters.php
+++ Monsters/monsters.php
@@ -8,7 +8,6 @@ $monsters = $query->fetchAll(PDO::FETCH_ASSOC);
 
 $query = $conn->query("SELECT l.name AS locale, l.id AS locale_id FROM `monsters` m JOIN monster_locales ml ON m.id = ml.monster_id JOIN locales l ON l.id = ml.locale_id WHERE m.id=" . $_GET['id']);
 $locales = $query->fetchAll(PDO::FETCH_ASSOC);
-
 ?>
 
 <!DOCTYPE html>
@@ -24,29 +23,36 @@ $locales = $query->fetchAll(PDO::FETCH_ASSOC);
         <div class="title">
             <img src="../Images/Monster Icons/<?=str_replace(" ", "_", $monster['name'])?>_Icon.webp" onerror="this.onerror=null; this.src='../Images/Monster Icons/Default_Icon.webp';" class="cover-image" alt="<?=$monster['name']?>" />
             <div class="header">
-                <h1><?=$monster['name']?></h1>
-                <h3><?=$monster['type']?></h3>
+                <h1 id="title"><?=$monster['name']?></h1>
+                <h3 id="type"><?=$monster['type']?></h3>
             </div>
         </div>
         <div class="index">
             <img src="../Images/Monster Renders/<?= str_replace(" ", "_", $monster['name'])?>_Render.webp" alt="" class="render">
-            <div class="descriptiondiv">
-                <i class="description">"<?= $monster['description']?>"</i>
-            </div>
-            <!-- <div class="relatedmonstersparent">
-            <?php 
-            $related_monsters = explode(", ", $monster['related_monsters']);
-            foreach ($related_monsters as $related_monster) {?>
-                <div class="relatedmonster">
-                    <h4><?= $related_monster?></h4>
+            <div class="index-left">  
+                <div class="descriptiondiv">
+                    <i class="description">"<?= $monster['description']?>"</i>
                 </div>
-            <?php }?>
-            </div> -->
+                <div class="relatedmonsterparent">
+                    <?php if (!empty($monster['related_monsters'])) {?>
+                    <h3>Related Monsters</h3>
+                <?php
+                $related_monsters = explode(", ", $monster['related_monsters']);
+                foreach ($related_monsters as $related_monster) {
+                $query = $conn->query("SELECT id FROM monsters WHERE name = '" . $related_monster . "'");
+                $monsterlink = $query->fetchAll(PDO::FETCH_ASSOC);?>
+                    <div class="relatedmonster">
+                        <a href="monsters.php?id=<?php foreach ($monsterlink as $index => $monster) {echo $monster['id'];}?>"><img src="../Images/Monster Icons/<?= str_replace(" ", "_", $related_monster)?>_Icon.webp" onerror="this.onerror=null; this.src='../Images/Monster Icons/Default_Icon.webp';" alt=""></a>
+                        <p><?= $related_monster?></p>
+                    </div>
+                <?php }}?>
+                </div>
+            </div>  
         </div>
         <div class="localeparent">
             <?php foreach ($locales as $index => $locale) {?>
                 <div class="localediv">
-                    <h2><?=$locale['locale']?></h2>
+                    <h2 id="locale"><?=$locale['locale']?></h2>
                     <img src="../Images/Locale Maps/<?= str_replace(" ", "_", $locale['locale'])?>" alt="" class="locales">
                 </div>
             <?php }?>
