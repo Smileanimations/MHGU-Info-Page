9ab98dfb28a98c08fc018a493cd027ecbe90c2f5
Roald <nihilzero28@gmail.com>
2024-11-06 09:34:21 +0100

    funny css sploinky

--- Locales/locales.php
+++ Locales/locales.php
@@ -13,6 +13,7 @@ $locales = $query->fetchAll(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="localeparent">
