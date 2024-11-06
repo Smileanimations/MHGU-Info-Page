080f5fcac8c6cbc226abfcbfd7ca86b43ebb2be6
Roald <nihilzero28@gmail.com>
2024-11-06 09:30:44 +0100

    added sql statement and printed out data

--- Locales/locales.php
+++ Locales/locales.php
@@ -1,3 +1,12 @@
+<?php
+
+include_once("../connection.php");
+include_once("../nav.html");
+
+$query = $conn->query("SELECT * FROM `locales`");
+$locales = $query->fetchAll(PDO::FETCH_ASSOC);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +15,13 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <div class="localeparent">
+        <?php foreach ($locales as $index => $locale) {?>
+            <div class="localediv">
+                <h2 id="locale"><?=$locale['name']?></h2>
+                <img src="../Images/Locale Maps/<?= str_replace(" ", "_", $locale['name'])?>.webp" alt="" class="locales">
+            </div>
+        <?php }?>
+    </div>
 </body>
 </html>
