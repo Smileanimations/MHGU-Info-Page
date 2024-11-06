beb355a2b963bac6863b1fc72a62b2f890ac852f
Mike29112001 <144684340+Mike29112001@users.noreply.github.com>
2024-11-06 11:54:32 +0100

    update Locales

--- Locales/locales.php
+++ Locales/locales.php
@@ -12,17 +12,17 @@ $locales = $query->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Locales</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="localeparent">
-        <?php foreach ($locales as $index => $locale) {?>
+        <?php foreach ($locales as $index => $locale) { ?>
             <div class="localediv">
-                <h2 id="locale"><?=$locale['name']?></h2>
-                <img src="../Images/Locale Maps/<?= str_replace(" ", "_", $locale['name'])?>.webp" alt="" class="locales">
+                <h2 id="locale"><?= htmlspecialchars($locale['name']) ?></h2> <!-- HTML special chars veilig weergeven -->
+                <img src="../Images/Locale Maps/<?= htmlspecialchars(str_replace(" ", "_", $locale['name'])) ?>.webp" alt="<?= htmlspecialchars($locale['name']) ?> map" class="locales">
             </div>
-        <?php }?>
+        <?php } ?>
     </div>
 </body>
 </html>
